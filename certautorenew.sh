028d34fa9ab9efe67faffdb6ccfa35c64436b7b1
IBLISS Labs <operacao@ibliss.com.br>
2018-02-14 16:42:55 -0300

    Create certautorenew.sh

---
+++ certautorenew.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Source: https://gist.github.com/oodavid/54cadfb92ff49618797d
+
+lepath="/usr/bin/letsencrypt"; #Change Letsencrypt path
+email="iblisslabs@ibliss.digital"; #Change email
+domain="$(/bin/hostname)"; #Change domain
+web_service="nginx"; #Change the webserver
+
+# Update the cert
+echo "Starting renewal script..."
+$le_path certonly -n --renew-by-default --nginx --agree-tos -m "$email" -d "$domain"
+
+# Reload nginx
+echo "Reloading $web_service"
+/usr/sbin/service $web_service reload
